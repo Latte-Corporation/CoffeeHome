7fd671c148905c356ae6af3cc27caeb049cf703a
Dorian TETU <doriantetu34@gmail.com>
2025-02-12 15:35:26 +0100

    refactor: reorganize utility functions and update styles; add button component

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
+        backgroundSidebar: "hsl(var(--background-sidebar))",
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
