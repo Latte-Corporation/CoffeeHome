80692977b4d7a86f188ea1ac75da6dc9f8287ca1
Dorian TETU <doriantetu34@gmail.com>
2025-02-12 16:20:18 +0100

    fix: fix the routing and improve the not-found page to match the AD
    Co-authored-by: Louis Labeyrie <Razano26@users.noreply.github.com>

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,19 +2,20 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16">
+    <div className="flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16 w-full h-full">
       <div className="w-full space-y-6 text-center">
         <div className="space-y-3">
-          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl transition-transform hover:scale-110">
+          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl transition-transform hover:scale-110 text-primary w-fit m-auto">
             404
           </h1>
-          <p className="text-gray-500">
-            Looks like you&apos;ve ventured into the unknown digital realm.
+          <p className="text-secondary">
+            Looks like you&apos;ve ventured into the unknown digital coffee
+            world.
           </p>
         </div>
         <Link
           href="/"
-          className="inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          className="inline-flex h-10 items-center rounded-md bg-primary px-8 text-sm font-medium text-white shadow transition-colors hover:bg-[#EDE0D4]/90 hover:text-secondary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
           prefetch={false}
         >
           Return to website
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { Button } from "@components/ui/button";
+import { BookOpenIcon, HomeIcon, UserIcon } from "@heroicons/react/24/outline";
+import { useRouter, usePathname } from "next/navigation";
+
+export default function Sidebar() {
+  const router = useRouter();
+  const pathname = usePathname().split("/")[1];
+
+  return (
+    <div className="flex flex-col min-w-64 bg-backgroundSidebar text-primary p-5">
+      <h1 className="text-xl">
+        <b>Latte Corporation</b>
+      </h1>
+      <div className="flex flex-col justify-between h-full">
+        <nav className="mt-10 flex flex-col gap-4">
+          <Button
+            className={`flex flex-row items-center justify-start gap-1 h-14`}
+            variant={pathname === "" ? "selected" : "default"}
+            onClick={() => router.push("/")}
+          >
+            <HomeIcon className="h-7 w-7" />
+            <h2 className="pl-3 text-base">Home</h2>
+          </Button>
+          <Button
+            className={`flex flex-row items-center justify-start gap-1 h-14`}
+            variant={pathname === "portfolio" ? "selected" : "default"}
+            onClick={() => router.push("/portfolio")}
+          >
+            <BookOpenIcon className="h-7 w-7" />
+            <h2 className="pl-3 text-base">Portfolio</h2>
+          </Button>
+          <Button
+            className={`flex flex-row items-center justify-start gap-1 h-14`}
+            variant={pathname === "contact" ? "selected" : "default"}
+            onClick={() => router.push("/contact")}
+          >
+            <UserIcon className="h-7 w-7" />
+            <h2 className="pl-3 text-base">Contact</h2>
+          </Button>
+        </nav>
+        <Button className="bg-[#E6CCB2] h-14">Request a demo</Button>
+      </div>
+    </div>
+  );
+}
