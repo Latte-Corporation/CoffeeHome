adb5390f7a04afde0c29a0bc68604266a0c5cc74
Louis Labeyrie <labeyrielouis@gmail.com>
2025-02-19 12:54:46 +0100

    feat: add animations to contact form and enhance UI with new components
    Signed-off-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,8 @@
 import MokaPot from "@components/3d/moka-pot";
 import ContactForm from "@components/contact-form";
+import { BoxReveal } from "@components/magicui/box-reveal";
+import { TextAnimate } from "@components/magicui/text-animate";
+import { BlurFade } from "@components/magicui/blur-fade";
 
 export default function Page() {
   return (
@@ -7,16 +10,24 @@ export default function Page() {
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 items-center h-full">
         <ContactForm />
         <div className="text-center lg:text-left hidden mx-auto xl:block">
-          <h2 className="text-3xl text-primary mb-4">
-            We&apos;re here to help
-          </h2>
-          <p className="text-secondary mb-6">
-            Our team is ready to bring your ideas to life. Let&apos;s create
-            something amazing together!
-          </p>
-          <div className="w-96 mx-auto h-96">
-            <MokaPot />
-          </div>
+          <BoxReveal boxColor={"hsl(30 52% 72%)"} duration={0.4}>
+            <h2 className="text-3xl text-primary mb-4">
+              We&apos;re here to help
+            </h2>
+          </BoxReveal>
+          <TextAnimate
+            animation="fadeIn"
+            by="line"
+            as="p"
+            className="text-secondary mb-6"
+          >
+            {`Our team is ready to bring your ideas to life.\nLet's create something amazing together!`}
+          </TextAnimate>
+          <BlurFade key="submit" delay={0.4} inView>
+            <div className="w-96 mx-auto h-96">
+              <MokaPot />
+            </div>
+          </BlurFade>
         </div>
       </div>
     </div>
