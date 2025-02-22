9f031933386f41606603842c1749cefefd5b6ebb
Dorian TETU <doriantetu34@gmail.com>
2025-02-22 16:38:34 +0100

    feat: add animated waves component and enhance sidebar layout

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,15 +2,19 @@
 import { Button } from "@components/ui/button";
 import { HomeIcon, UserIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter } from "next/navigation";
+import Waves from "./ui/waves";
 
 export default function Sidebar() {
   const router = useRouter();
   const pathname = usePathname().split("/")[1];
 
   return (
-    <div className="flex flex-col min-w-64 bg-backgroundSidebar text-primary p-5">
-      <h1 className="text-xl font-bold">Latté</h1>
-      <div className="flex flex-col justify-between h-full">
+    <div className="flex flex-col min-w-64 bg-backgroundSidebar text-primary">
+      <div className="bg-primary w-full p-5">
+        <h1 className="text-xl font-bold text-[#EDE0D4]">Latté</h1>
+      </div>
+      <Waves />
+      <div className="flex flex-col justify-between h-full px-5 pb-5">
         <nav className="mt-10 flex flex-col gap-4">
           <Button
             className={`flex flex-row items-center justify-start gap-1 h-14`}
