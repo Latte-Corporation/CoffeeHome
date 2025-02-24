0a7495870cf05935e7f5aabd2c5e74af88563967
Dorian TETU <doriantetu34@gmail.com>
2025-02-24 15:41:29 +0100

    feat: add desktop and mobile page components with responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import MobilePage from "./-/mobile-page/page";
-import DesktopPage from "./-/desktop-page/page";
+import MobilePage from "./-/mobile-page";
+import DesktopPage from "./-/desktop-page";
 
 export default function Page() {
   return (
